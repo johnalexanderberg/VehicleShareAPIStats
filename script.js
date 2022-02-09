72ffde4894c85e8f06cea2edd29cbabe04d724ad
alexanderberg <alexander.berg1@learnet.se>
2022-02-09 17:32:24 +0100

    clean up the code

--- script.js
+++ script.js
@@ -1,47 +1,131 @@
-console.log("hej")
-
+//API Endpoint
 const siteUrl = "https://localhost:44323";
+
 const svgNameSpace = "http://www.w3.org/2000/svg";
 
-const section = document.getElementById("stats");
-console.log(section);
-//
-// fetch(siteUrl + "/api/stats")
-//     .then(response => response.json())
-//     .then(data => renderString(data));
-//
-// function renderString({userCount, vehicleCount, firstBooking, bookingCount}) {
-//     section.textContent = (userCount < 1 ? userCount + " user has shared" : userCount +" users have shared ")
-//         + vehicleCount + " " + (vehicleCount < 1 ? "vehicle " : "vehicles ") + "since " + firstBooking + " and completed " + bookingCount +" rides";
-// }
+const todayText = document.getElementById("todayText");
 
-//select Today Svg
+//Size of svg and offset between bars
 const svgWidth = 600;
 const svgHeight = 200;
 const rectOffset = 1;
-const data1 = [123, 245, 532, 3, 56, 654, 43, 36, 65, 34, 56, 234, 455, 65, 45, 465, 45, 23, 64, 34];
-const vehiclesData = [123, 245, 532, 3, 56, 654, 43, 36];
 
+function calculateTodayData(bookings){
 
-const todaySvgContainer = document.getElementById("todaySvg");
-createSvg(svgWidth, svgHeight, rectOffset, data1, todaySvgContainer)
+    let vehicles = {
+        Car : 0,
+        Bike: 0,
+        Boat: 0,
+        Airplane: 0,
+        SpaceShip: 0,
+        FlyingBroom: 0,
+        FlyingCarpet: 0,
+        TimeMachine: 0,
+    }
 
-const vehiclesSvgContainer = document.getElementById("vehiclesSvg");
-createSvg(svgWidth, svgHeight, rectOffset, vehiclesData, vehiclesSvgContainer)
+    let bookingsHours = [
+        ["00", 0],
+        ["01", 0],
+        ["02", 0],
+        ["03", 0],
+        ["04", 0],
+        ["05", 0],
+        ["06", 0],
+        ["07", 0],
+        ["08", 0],
+        ["09", 0],
+        ["10", 0],
+        ["11", 0],
+        ["12", 0],
+        ["13", 0],
+        ["14", 0],
+        ["15", 0],
+        ["16", 0],
+        ["17", 0],
+        ["18", 0],
+        ["19", 0],
+        ["20", 0],
+        ["21", 0],
+        ["22", 0],
+        ["23", 0],
+    ];
 
+    bookings.forEach(function ({startTime, endTime, vehicleType}){
+        let bookingStartHr = startTime.slice(11, 13);
+        if (bookingStartHr[0] === 0){
+            bookingStartHr = bookingStartHr[1]
+        }
+        let bookingEndHr = endTime.slice(11, 13);
+        if (bookingEndHr[0] === 0){
+            bookingEndHr = bookingStartHr[1]
+        }
+        for(let i = 0; i < 24; i++){
+            if (bookingStartHr <= i && bookingEndHr >= i){
+                bookingsHours[i][1] += 1;
+            }
+        }
+        vehicles[vehicleType] ++;
+    })
 
-const todaySvgLabels = document.createElement("div");
+    console.log(vehicles);
 
+    const data = [bookingsHours, [
+        ["Car", vehicles.Car],
+        ["Bike", vehicles.Bike],
+        ["Boat", vehicles.Boat],
+        ["Airplane", vehicles.Airplane],
+        ["Car", vehicles.SpaceShip],
+        ["Flying Broom", vehicles.FlyingBroom],
+        ["Flying Carpet", vehicles.FlyingCarpet],
+        ["Time Machine", vehicles.TimeMachine],
+        ]]
+    return data;
+}
 
-todaySvgLabels.classList.add("todaySvgLabels");
-todaySvgContainer.append(todaySvgLabels);
+//This function gets statistics from server and create svg graphics from the data.
+async function runTodayStats(){
+    const todayResponse = await fetch(siteUrl + "/api/lastTwentyFourHours");
+    const todayJson = await todayResponse.json();
 
+    const data = calculateTodayData(todayJson);
+    const bookingsHours = data[0];
+    const todaySvgContainer = document.getElementById("todaySvg");
+    createSvg(svgWidth, svgHeight, rectOffset, bookingsHours, todaySvgContainer)
 
+    const vehiclesSvgContainer = document.getElementById("vehiclesSvg");
+    createSvg(svgWidth, svgHeight, rectOffset, data[1], vehiclesSvgContainer)
 
+    const allrect = document.querySelectorAll('rect');
+//show popup label on mouse enter, and add text stored in rect
+    allrect.forEach(r => r.addEventListener('mouseenter', function(e){
+        popup.classList.add("visible");
+        popup.textContent = e.target.textContent;
+    }))
 
+    allrect.forEach(r => r.addEventListener('mouseout', function(e){
+        popup.classList.remove("visible");
+    }))
+}
+
+function renderString({userCount, vehicleCount, firstBooking, bookingCount}) {
+
+    todayText.textContent = (userCount < 1 ? userCount + " user has shared" : userCount +" users have shared ")
+        + vehicleCount + " " + (vehicleCount < 1 ? "vehicle " : "vehicles ") + "since " + firstBooking.slice(0, 10) + " and completed " + bookingCount +" rides";
+}
+
+async function runGeneralStats(){
+    const statsResponse = await fetch(siteUrl + "/api/stats");
+    const statsJson = await statsResponse.json();
+    renderString(statsJson);
+}
 
 function createSvg(svgWidth, svgHeight, rectOffset, data, container) {
-    const dataMax = Math.max(...data);
+    let dataMax = 0;
+    for (let i = 0; i < data.length; i++){
+        if (data[i][1] > dataMax)
+        dataMax = data[i][1];
+    }
+    console.log(dataMax)
     //create the svg
     const svg = document.createElementNS(svgNameSpace, "svg");
     svg.setAttribute("width", svgWidth);
@@ -52,20 +136,21 @@ function createSvg(svgWidth, svgHeight, rectOffset, data, container) {
     labelContainer.setAttribute("width", svgWidth);
     labelContainer.setAttribute("height", "auto");
 
-
     data.forEach((d, i) => {
 
+        const label = d[0];
+        const value = d[1];
 
-
+        console.log("label: " +label + " value: " +value + " dataMax: " +dataMax);
 
         //scale data value to svg height
-        const rectHeight = svgHeight/dataMax*d;
+        const rectHeight = svgHeight/dataMax*value;
         // create the rect that will visualize the data
         const rectTag = document.createElementNS(svgNameSpace, "rect");
 
+        // store position of rectangle, account for svgWidth and offset
         const xPos =  ((svgWidth -(rectOffset * data.length))/data.length * i) + (rectOffset * i);
 
-        // position rectangle, account for svgWidth and offset
         rectTag.setAttribute("x", xPos);
         // because 0 is the top of the svg and not the bottom, we need to push the rect down its own height
         rectTag.setAttribute("y", svgHeight-rectHeight);
@@ -78,45 +163,40 @@ function createSvg(svgWidth, svgHeight, rectOffset, data, container) {
         //make rounded corner
         rectTag.setAttribute("rx", "10");
 
-        //create label for the rect
+        //create label showing the data value
         const rectLabel = document.createElement("text")
+        rectLabel.textContent = value;
         rectLabel.classList.add("rectLabel");
         rectTag.append(rectLabel);
 
-        //add the rect to the svg and return it
+        //add the rect to the svg
         svg.append(rectTag);
 
-        const dataLabel = document.createElement("p");
-        dataLabel.textContent = i;
-        dataLabel.classList.add("dataLabel");
-        dataLabel.setAttribute("style", "left:" +xPos+"px")
-
-        // "left:" +xPos+"px")
-
-        labelContainer.append(dataLabel);
+        //create the label showing the title
+        const labelElement = document.createElement("div");
+        labelElement.classList.add("labelName");
+        labelElement.style.width = (svgWidth / data.length) + "px";
+        labelElement.style.overflow = "hidden";
+        labelElement.textContent = label;
+        labelContainer.append(labelElement);
 
     });
     container.append(svg)
-    // container.append(labelContainer)
-
-
-
-
-};
-function createSvgLabels(svgWidth, svgHeight, rectOffset, data) {
+    container.append(labelContainer)
 
 };
 
-const allRects = document.querySelectorAll('rect');
+//popup label
+const popup = document.getElementById('popup');
 
-allRects.forEach(r => r.addEventListener("click", function (e)
-{
-console.log(e);
-}))
+//make popup follow mouse movement
+window.addEventListener('mousemove', function(e) {
+    const left = e.clientX;
+    const top = e.clientY + 25;
+    popup.style.left = left + 'px';
+    popup.style.transform = "translate(-50%, 0)"
+    popup.style.top = top + 'px';
+});
 
-
-
-
-//select all of the rect tags
-
-//change the width
+runTodayStats();
+runGeneralStats();
